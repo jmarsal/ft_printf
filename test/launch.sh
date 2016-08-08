468f4fe2467c1ec7e65b830358a6b2e6ae7c1697
jmarsal <jmarsal@student.42.fr>
2016-08-09 01:33:46 +0200

    Divers ajustement graphique

--- test/launch.sh
+++ test/launch.sh
@@ -1,8 +1,7 @@
 cd ..
 make re
-cp test/test1.c ./
-gcc -Wall -Wextra -Werror test1.c -o ft_printf_test -I./includes libftprintf.a
-echo "\n\033[34mDebut de test \n\n\033[0m" 
+gcc -Wall -Wextra -Werror test/test1.c -o ft_printf_test -I./includes libftprintf.a
+echo "\n\033[34mDebut de test \n\n\033[0m"
 ./ft_printf_test
 rm ft_printf_test
 echo "\n\033[31mFin de test \n\n\033[0m"
