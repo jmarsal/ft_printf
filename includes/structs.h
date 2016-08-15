0b0082530a22e3595daead10fb404d63db4da565
jmarsal <jmarsal@student.42.fr>
2016-08-16 01:56:48 +0200

    Quelques commentaires
    --> + suppression de fichier inutiles

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 16:59:05 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/08/12 00:37:01 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/08/16 01:53:00 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 
 typedef struct		s_conversion
 {
-	char			l_conv;
+	char			l_conv; //le flag de conversion. Peut etre besoin d'un char *
+							//Besoin d'une variable de taille de champ
+							//Besoin d'une variable de precision (par default 1)
+							//Besoin d'une variable modificateur de longueur (hh, h, l, ll, etc...)
 }					t_conversion;
 
 typedef struct		s_v_args
 {
-	t_conversion	*f_conv;
-	int				index;
+	t_conversion	*f_conv; //stoque les flags de format dans l'ordre dans un tab
+	int				index; // nb d'args
 }					t_v_args;
 
 #endif
