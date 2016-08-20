1b7747ee26a148cf902266285a2e8a51c81fa3ec
jmarsal <jmarsal@student.42.fr>
2016-08-21 00:33:00 +0200

    Le parser a l'air de fonctionner
    --> tous mes tests passent
    --> A voir avec la suite des flags
    --> Je merge sur master et passe sur la branche
    	output

--- includes/structs.h
+++ includes/structs.h
@@ -6,16 +6,21 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/11 16:59:05 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/08/16 01:53:00 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/08/21 00:10:36 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
+# include "ft_printf.h"
+
 typedef struct		s_conversion
 {
-	char			l_conv; //le flag de conversion. Peut etre besoin d'un char *
+	char			*str;
+	int				nb;
+	// size_t			index;
+	// char			l_conv; //le flag de conversion. Peut etre besoin d'un char *
 							//Besoin d'une variable de taille de champ
 							//Besoin d'une variable de precision (par default 1)
 							//Besoin d'une variable modificateur de longueur (hh, h, l, ll, etc...)
@@ -23,7 +28,8 @@ typedef struct		s_conversion
 
 typedef struct		s_v_args
 {
-	t_conversion	*f_conv; //stoque les flags de format dans l'ordre dans un tab
+	t_conversion	**f_conv; //stoque les flags de format dans l'ordre dans un tab
+	size_t			i_args;
 	int				index; // nb d'args
 }					t_v_args;
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/19 01:24:50 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/08/20 15:41:31 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/08/21 00:27:19 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int main()
 {
-	ft_printf("%s%s", "Hello World !\n");
-	// ft_printf("\n\n%s%s", "Hello World !\n");
-	// ft_printf("\n\n%s%s%%%%%s", "Hello World !\n"); //PB !!
+	// ft_printf("Hello%s%s world\n", " World !\n", "I am so happy");
+	// ft_printf("\n\n%s%s", "Hello World !\n", "C'est la fete !\n");
+	// ft_printf("\n\n%s%s%%%%%s", "Justine st jolie\n", "Antoine aussi!\n", "mon ange evidemment !\n");
 	// ft_printf("%%%s", "Hello World !\n");
 	// ft_printf("%%\n");
 	// ft_printf("Hello World\n");
-	// ft_printf("%D\n", 42);
+	ft_printf("%d\n", 42);
 	return (0);
 }
